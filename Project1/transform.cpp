1c821c9b817194483c48a21483b54bc4eff64297
netease-ljy <18868803201@163.com>
2017-12-25 22:16:36 +0800

    摄像机修改

--- Project1/transform.cpp
+++ Project1/transform.cpp
@@ -13,6 +13,23 @@
 const int w_width = 800;
 const int w_height = 600;
 
+double mouse_x = w_width / 2;
+double mouse_y = w_height / 2;
+
+//指的是摄像机的指向
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -3.0f);
+
+// 俯仰角
+double pitch = 0.0;
+//偏航角
+double yaw = 0.0;
+
+double senstive = 0.5;
+
+bool firstMouse = true;
+
+bool last_update_tag = false;
+
 void windows_size_chg(GLFWwindow *window, int width, int height) {
 	glfwSetWindowSize(window, width, height);
 	glViewport(0, 0, width, height);
@@ -25,6 +42,7 @@ void input_process(GLFWwindow *window) {
 		glfwSetWindowShouldClose(window, true);
 		return;
 	}
+	/*
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
 		printf("asdasdad");
 		camera_pos.y -= 0.01;
@@ -41,15 +59,39 @@ void input_process(GLFWwindow *window) {
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
 		camera_pos.x += 0.01;
 		return;
-	}
+	}*/
 }
 
-void load_texture(const char *pth, std::string& image_buff,int &width, int &height, int& channel) {
-	char * buff = (char *)stbi_load(pth, &width, &height, &channel, 0);
-	if (buff == NULL) {
-		return;
+void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
+	//printf("%.5lf %.5lf\n", xpos, ypos);
+	if (firstMouse) // 这个bool变量初始时是设定为true的
+	{
+		mouse_x = xpos;
+		mouse_y = ypos;
+		firstMouse = false;
 	}
-	image_buff = buff;
+	double delta_x = xpos - mouse_x;
+	// 屏幕右下的顶点时(width, height)
+	double delta_y = mouse_y - ypos;
+	mouse_x = xpos;
+	mouse_y = ypos;
+
+	pitch += delta_y * senstive;
+	yaw += delta_x * 0.3;
+	if (pitch >= 89.0) {
+		pitch = 89.0;
+	}
+	else if (pitch <= -89.0) {
+		pitch = -89.0;
+	}
+
+	glm::vec3 diretion;
+	diretion.x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
+	diretion.y = 0;//sin(glm::radians(pitch));
+	diretion.z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
+	diretion = glm::normalize(diretion);
+	printf("%.3lf %.3lf %.3lf %.3lf\n", pitch, yaw, cos(glm::radians(pitch)) * cos(glm::radians(yaw)), cos(glm::radians(pitch)) * sin(glm::radians(yaw)));
+	cameraFront = diretion;
 }
 
 int main() {
@@ -73,6 +115,8 @@ int main() {
 		return -1;
 	}
 	glfwSetFramebufferSizeCallback(window, windows_size_chg);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	//视口坐标
 	//glViewport(0, 0, w_width, w_height);
 	float vertex[] = {
@@ -196,6 +240,12 @@ int main() {
 		glClearColor(0.5, 0.5, 0.5, 1);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glBindVertexArray(VAO);
+		shader.use_program();
+		// 第二个参数表示目标位置
+		// eye direction up
+		view = glm::lookAt(camera_pos, -camera_pos + cameraFront, glm::vec3(0.0, 1.0, 0.0));
+		//printf("%.3lf\n", cameraFront.y);
+		shader.set_uniform_matrix_4fv("view", view);
 		for (int index = 0; index < 10; ++index) {
 			glm::mat4 model;
 			glm::mat4 trans;
@@ -209,9 +259,6 @@ int main() {
 			shader.set_uniform_matrix_4fv("model", model);
 			float radius = 10.0f;
 			glm::mat4 view;
-			view = glm::lookAt(camera_pos, glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
-			shader.set_uniform_matrix_4fv("view", view);
-			shader.use_program();
 			// 注意顶点着色器每次都会以一个单独的顶点作为输入
 			// 该顶点包含的数据叫做顶点属性(如上述 为顶点的xyz和对应的纹理坐标)
 			glDrawArrays(GL_TRIANGLES, 0, 36);
